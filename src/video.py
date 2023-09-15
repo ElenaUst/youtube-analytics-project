e9980c4a0f49080f75acd886f9699df39e5fe901
ElenaUst <elka_us@mail.ru>
2023-09-15 21:52:43 +0300

    add exceptions

--- src/video.py
+++ src/video.py
@@ -3,7 +3,7 @@ from googleapiclient.discovery import build
 from pprint import pprint
 
 
-class Video():
+class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
@@ -12,17 +12,27 @@ class Video():
         """
         self.video_id = video_id
         self.video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                          id=video_id).execute()
-        # pprint(self.video_response)
+                                                              id=self.video_id).execute()
+        #pprint(self.video_response)
 
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.url = "https://www.youtube.com/" + self.video_response.get('items')[0].get('etag')
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
-        self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
+        try:
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+
+        except IndexError:
+            print('Incorrect_video_id')
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+            self.comment_count = None
+        else:
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.url = "https://www.youtube.com/" + self.video_response.get('items')[0].get('etag')
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+            self.comment_count: int = self.video_response['items'][0]['statistics']['commentCount']
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
@@ -36,4 +46,4 @@ class PLVideo(Video):
                                                                     part='contentDetails',
                                                                     maxResults=50,
                                                                     ).execute()
-        # pprint(self.playlist_videos)
+
